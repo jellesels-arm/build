0a5029db21baa62aac24543727312b7e667139f2
Gyorgy Szing <Gyorgy.Szing@arm.com>
2020-11-06 00:33:49 +0100

    Add FF-A SP build targets.
    Add make targets to allow building the FF-A Secure Partitions from the
    Trusted Services repository.
    
    Change-Id: I41724aae9e921ce11aa6c0035e8b30bfcd46c9f2
    Signed-off-by: Gyorgy Szing <Gyorgy.Szing@arm.com>

--- fvp_ffa.mk
+++ fvp_ffa.mk
@@ -16,3 +16,53 @@ TF_A_FLAGS ?= \
 	SPMD_SPM_AT_SEL2=0
 
 include fvp.mk
+
+TS_INSTALL_PREFIX:=$(CURDIR)/../out-ts
+
+# Add machinery allowing to build secure partitions from Trusted Services.
+#
+# build-sp <sp-name>
+#   <sp name>   The name of the SP.
+#
+# When called build and clean targets for the SP will be defined as:
+#
+#   ffa-<sp name>-sp            - Build the SP with cmake, and include the SP
+#                                 export makefile to make the SP binary part
+#                                 of the OP-TEE OS image.
+#   ffa-<sp name>-sp-clean      - run make clean on the cmake project
+#   ffa-<sp name>-sp-realclean  - remove all cmake output
+#
+# The build and the clean target are added to the dependency tree of common
+# op-tee targets.
+#
+
+define build-sp
+.PHONY: ffa-$1-sp
+ffa-$1-sp: ffa-$1-sp-build
+	$$(eval include $${TS_INSTALL_PREFIX}/lib/make/$1.mk)
+
+.PHONY: ffa-$1-sp-build
+ffa-$1-sp-build: optee-os-spdevkit
+	CROSS_COMPILE="$$(AARCH64_CROSS_COMPILE)" cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$${TS_INSTALL_PREFIX} \
+		-DSP_DEV_KIT_DIR=$$(CURDIR)/../optee_os/out/arm/export-sp_arm64 \
+		-S $$(CURDIR)/../trusted-services/deployments/$1/opteesp -B $$(CURDIR)/../ts-build/$1
+	cmake --build $$(CURDIR)/../ts-build/$1 -- -j$$(nproc)
+	cmake --install $$(CURDIR)/../ts-build/$1
+
+.PHONY: ffa-$1-sp-clean
+ffa-$1-sp-clean:
+	cmake --build $$(CURDIR)/../ts-build/$1 -- clean -j$$(nproc)
+
+ffa-$1-sp-realclean:
+	rm -rf $$(CURDIR)/../ts-build/$1
+
+ffa-sp-realclean: ffa-$1-sp-realclean
+
+optee-os-common: ffa-$1-sp
+optee-os-clean: ffa-$1-sp-clean
+
+endef
+
+$(eval $(call build-sp,secure-storage))
+$(eval $(call build-sp,crypto))
+
